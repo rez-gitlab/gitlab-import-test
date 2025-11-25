b2797f502f51967893e36be16a0fa0b1d17fc5c0
Rez <rmarandi@gitlab.com>
2025-11-25 12:52:22 +0100

    Add app/controllers/users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
